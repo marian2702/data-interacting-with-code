98e4b36d64bef4bbce9869c44d51834cf10a2cbd
Halibei Marian <marianhalibei123@gmail.com>
2023-07-20 17:51:17 +0200

    Solve the  SQL and Python 1 exercise of the day

--- queries.py
+++ queries.py
@@ -1,27 +1,64 @@
-# pylint: disable=missing-docstring, C0103
+import sqlite3
+
+conn = sqlite3.connect('data/movies.sqlite')
+db = conn.cursor()
+db.execute("SELECT * FROM directors")
+rows = db.fetchall()
+print(rows)
+# => list (rows) of tuples (columns)
 
 def directors_count(db):
-    # return the number of directors contained in the database
-    pass  # YOUR CODE HERE
+    query = "SELECT COUNT(*) AS total_directors FROM directors"
 
+    result = db.execute(query)
+    result = db.fetchone()
+    return result[0]
 
 def directors_list(db):
-    # return the list of all the directors sorted in alphabetical order
-    pass  # YOUR CODE HERE
+    query = "SELECT * FROM directors ORDER BY name ASC"
+
+    result = db.execute(query)
+    director_names = [row[0] for row in result.fetchall()]
+    return director_names
 
 
 def love_movies(db):
-    # return the list of all movies which contain the exact word "love"
-    # in their title, sorted in alphabetical order
-    pass  # YOUR CODE HERE
+    query = """
+        SELECT title
+        FROM movies
+        WHERE UPPER(title) LIKE '% LOVE %'
+        OR UPPER(title) LIKE 'LOVE %'
+        OR UPPER(title) LIKE '% LOVE'
+        OR UPPER(title) LIKE 'LOVE'
+        OR UPPER(title) LIKE '% LOVE''%'
+        OR UPPER(title) LIKE '% LOVE.'
+        OR UPPER(title) LIKE 'LOVE,%'
+        ORDER BY title
+    """
+
+    result = db.execute(query)
+    movie_titles = [row[0] for row in result.fetchall()]
+
+    return movie_titles
 
 
 def directors_named_like_count(db, name):
+    query = """SELECT COUNT(*) FROM directors WHERE LOWER(name) LIKE ?"""
+
+    db.execute(query, (f"%{name}%",))
+    count = db.fetchone()
+    return count[0]
     # return the number of directors which contain a given word in their name
-    pass  # YOUR CODE HERE
+
 
 
 def movies_longer_than(db, min_length):
-    # return this list of all movies which are longer than a given duration,
-    # sorted in the alphabetical order
-    pass  # YOUR CODE HERE
+    query = """
+        SELECT title
+        FROM movies
+        WHERE minutes > ?
+        ORDER BY title
+    """
+    db.execute(query, (min_length,))
+    movies = db.fetchall()
+    return [movie[0] for movie in movies]
